247fa8f83da644f2cde132136bced10807e4df87
Nate Wright <natew@notthisway.com>
2016-06-30 12:06:18 +0100

    pkp/pkp-lib#1468 Refactor to use new assets API

--- TinyMCEPlugin.inc.php
+++ TinyMCEPlugin.inc.php
@@ -26,7 +26,8 @@ class TinyMCEPlugin extends GenericPlugin {
 	function register($category, $path) {
 		if (parent::register($category, $path)) {
 			if ($this->getEnabled()) {
-				HookRegistry::register('TemplateManager::display',array(&$this, 'callback'));
+				HookRegistry::register('TemplateManager::display',array(&$this, 'registerJS'));
+				HookRegistry::register('TemplateManager::registerJSLibraryData',array(&$this, 'registerJSData'));
 			}
 			return true;
 		}
@@ -59,26 +60,54 @@ class TinyMCEPlugin extends GenericPlugin {
 	}
 
 	/**
-	 * Hook callback function for TemplateManager::display
+	 * Register the TinyMCE JavaScript file
+	 *
+	 * Hooked to the the `display` callback in TemplateManager
 	 * @param $hookName string
 	 * @param $args array
 	 * @return boolean
 	 */
-	function callback($hookName, $args) {
+	function registerJS($hookName, $args) {
 		$request =& Registry::get('request');
 		$templateManager =& $args[0];
 
-		$allLocales = AppLocale::getAllLocales();
-		$localeList = array();
-		foreach ($allLocales as $key => $locale) {
-			$localeList[] = PKPString::substr($key, 0, 2);
+		// Load the TinyMCE JavaScript file
+		$min = Config::getVar('general', 'enable_minified') ? '.min' : '';
+		$templateManager->addJavaScript(
+			'tinymce',
+			$request->getBaseUrl() . '/lib/pkp/lib/vendor/tinymce/tinymce/tinymce' . $min . '.js',
+			array(
+				'contexts' => 'backend',
+			)
+		);
+
+		return false;
+	}
+
+	/**
+	 * Register script data required by the JS library
+	 *
+	 * Hooked to the the `registerJSLibraryData` callback in TemplateManager.
+	 * This data is used to initialize the TinyMCE component.
+	 * @param $hookName string
+	 * @param $args array $args[0] is an array of plugin data.
+	 * @return boolean
+	 */
+	function registerJSData($hookName, $args) {
+		$request =& Registry::get('request');
+
+		$tinymceParams = array();
+
+		$localeKey = substr(AppLocale::getLocale(), 0, 2);
+		$localePath = $request->getBaseUrl() . '/plugins/generic/tinymce/langs/' . $localeKey . '.js';
+
+		if (file_exists($localePath)) {
+			$tinymceParams['language']     = $localeKey;
+			$tinymceParams['language_url'] = $localePath;
 		}
 
-		$templateManager->assign(
-			'additionalHeadData',
-			$templateManager->get_template_vars('additionalHeadData') .
-			$templateManager->fetch($this->getTemplatePath() . 'header.tpl')
-		);
+		$args[0][$this->getJavascriptNameSpace()] = array( 'tinymceParams' => json_encode($tinymceParams) . ';');
+
 		return false;
 	}
 
